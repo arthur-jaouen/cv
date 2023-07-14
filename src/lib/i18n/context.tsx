dc5c568b4e1580b9e8ee3a03d0f4d30661271d52
Arthur JAOUEN <arthur.jaouen@gmail.com>
2023-07-14 15:32:30 +0100

    Fix issue with language not initializing correctly

--- src/lib/i18n/context.tsx
+++ src/lib/i18n/context.tsx
@@ -7,22 +7,20 @@ import {
   useEffect,
   useState,
 } from 'react';
-import { Language, languageFromString } from './language';
+import { getBrowserLanguage, Language } from './language';
 
-export const LanguageContext = createContext<Language>(
-  languageFromString(window.navigator.language),
-);
+export const LanguageContext = createContext<Language>(getBrowserLanguage());
 
 export function useLanguage(): Language {
   return useContext(LanguageContext);
 }
 
 export const LanguageProvider: FunctionComponent<PropsWithChildren> = ({ children }) => {
-  const [language, setLanguage] = useState(languageFromString(window.navigator.language));
+  const [language, setLanguage] = useState(getBrowserLanguage());
 
   useEffect(() => {
     function hashChanged() {
-      const language = languageFromString(window.location.hash.substring(1));
+      const language = getBrowserLanguage();
       dayjs.locale(language);
       setLanguage(language);
     }
--- src/lib/i18n/language.tsx
+++ src/lib/i18n/language.tsx
@@ -3,3 +3,9 @@ export type Language = 'fr' | 'en' | 'es';
 export function languageFromString(lang: string): Language {
   return (['fr', 'en', 'es'].find((l) => lang.startsWith(l)) as Language) || 'en';
 }
+
+export function getBrowserLanguage(): Language {
+  const hash = window.location.hash.substring(1);
+
+  return languageFromString(hash.length > 0 ? hash : window.navigator.language);
+}
